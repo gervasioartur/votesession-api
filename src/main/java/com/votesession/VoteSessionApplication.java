373d2ea15c7878bcfd8187592e5966434e9b4e7d
gervasioartur <gervasioartur@outlook.com.br>
2024-09-24 18:40:36 -0300

    chore: setup the project

---
+++ src/main/java/com/votesession/VoteSessionApplication.java
@@ -0,0 +1,12 @@
+package com.votesession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteSessionApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(VoteSessionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/votesession/VoteSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.votesession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteSessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
