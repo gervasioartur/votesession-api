899c2c6b9fe7c88283315ae0ef88579ae8c3c987
gervasioartur <gervasioartur@outlook.com.br>
2024-09-24 22:36:15 -0300

    docs: add documentation for create new agenda endpoint with swagger

--- src/main/java/com/votesession/api/controller/AgendaController.java
+++ src/main/java/com/votesession/api/controller/AgendaController.java
@@ -4,6 +4,10 @@ import com.votesession.api.dto.CreateAgendaRequest;
 import com.votesession.api.dto.Response;
 import com.votesession.domain.Agenda;
 import com.votesession.service.contracts.AgendaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -16,13 +20,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/agendas")
 @RequiredArgsConstructor
+@Tag(name = "Agendas")
+@RequestMapping("/agendas")
 public class AgendaController {
     private final AgendaService service;
     private final ModelMapper mapper;
 
+    @Operation(summary = "Create new Agenda")
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Returns successful message"),
+            @ApiResponse(responseCode = "400", description = "Bad request happened"),
+            @ApiResponse(responseCode = "500", description = "An unexpected error occurred."),
+    })
     public ResponseEntity<Response> create(@Valid @RequestBody CreateAgendaRequest request) {
         Agenda agenda = this.mapper.map(request, Agenda.class);
         this.service.create(agenda);
