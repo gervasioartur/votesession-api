6588d38ccefa9b1dc9947dfdfb98fab9ecfb3a6c
gervasioartur <gervasioartur@outlook.com.br>
2024-09-25 19:57:30 -0300

    feat: configuring webclient

--- src/main/java/com/votesession/service/impl/UserServiceImpl.java
+++ src/main/java/com/votesession/service/impl/UserServiceImpl.java
@@ -1,12 +1,19 @@
 package com.votesession.service.impl;
 
 import com.votesession.service.contracts.UserService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class UserServiceImpl implements UserService {
     @Override
     public boolean isAbleToVote(String document) {
         return false;
+    }
+
+    @Bean
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
     }
 }
