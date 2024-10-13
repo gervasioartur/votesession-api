62b6b4e1f58562bfea5b694684814804a44efdba
gervasioartur <gervasioartur@outlook.com.br>
2024-10-13 15:41:31 -0300

    chore: add  dev deploy

---
+++ infra/varialbles.tf
@@ -0,0 +1,81 @@
+# Database instance variables
+variable "database_allocated_storage" {
+   type = number
+}
+
+variable "database_max_allocated_storage" {
+  type = number
+}
+
+variable "database_engine_version" {
+  type = string
+}
+
+variable "database_instance_class" {
+  type = string
+}
+
+variable "database_name" {
+  type = string
+}
+
+variable "database_password" {
+  type = string
+}
+
+variable "database_username" {
+  type = string
+}
+
+variable "database_parameter_group_name" {
+  type = string
+}
+
+variable "database_instance_name" {
+  type = string
+}
+
+# Elastic Redis Cache
+variable "redis_cluster_id" {
+  type = string
+}
+
+variable "redis_node_type" {
+  type = string
+}
+
+variable "redis_cluster_name" {
+  type = string
+}
+
+# EC2 variables
+variable "deployer_key_name" {
+  type = string
+}
+
+variable "ami_instance" {
+  type = string
+}
+variable "ami_instance_type" {
+  type = string
+}
+
+variable "docker_username" {
+  type = string
+}
+
+variable "docker_password" {
+  type = string
+}
+
+variable "image_name" {
+  type = string
+}
+
+variable "image_tag" {
+  type = string
+}
+
+variable "server_instance_name" {
+  type = string
+}
