771ebe31545738ef5737aa0636652965fdb88859
Gervásio Artur Dombo <90800218+gervasioartur@users.noreply.github.com>
2024-10-14 20:52:29 -0300

    change

--- infra/main.tf
+++ infra/main.tf
@@ -50,7 +50,7 @@ resource "tls_private_key" "ssh_key" {
 
 resource "aws_key_pair" "deployer" {
   key_name = var.deployer_key_name
-  public_key = tls_private_key.ssh_key.public_key_openssh
+  public_key = file("~/.ssh/id_rsa.pub")
 }
 
 # Security group to allow SSH and HTTP access on EC2 instance
