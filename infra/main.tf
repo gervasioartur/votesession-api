147afc596e64fba81eeeefde09f99d78ffbaec04
Gervásio Artur Dombo <90800218+gervasioartur@users.noreply.github.com>
2024-10-15 02:58:28 -0300

    test

--- infra/main.tf
+++ infra/main.tf
@@ -12,13 +12,6 @@ resource "aws_s3_bucket_public_access_block" "bucket" {
   restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_ownership_controls" "bucket" {
-  bucket = aws_s3_bucket.bucket.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
 resource "aws_s3_bucket_acl" "bucket" {
   depends_on = [
 	aws_s3_bucket_public_access_block.bucket,
