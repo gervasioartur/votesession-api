e935a7acaf899fda626cff4d21bea4b95f0fdb70
Gervásio Artur Dombo <90800218+gervasioartur@users.noreply.github.com>
2024-10-14 22:58:27 -0300

    bkcuet

--- infra/main.tf
+++ infra/main.tf
@@ -1,7 +1,32 @@
 # Create S3 bucket
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
-  acl = "public-read"
+}
+
+resource "aws_s3_bucket_public_access_block" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_ownership_controls" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "bucket" {
+  depends_on = [
+	aws_s3_bucket_public_access_block.bucket,
+	aws_s3_bucket_ownership_controls.bucket,
+  ]
+
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "public-read"
 }
 
 # Security group to allow access to RDS
